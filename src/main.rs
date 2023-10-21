dd1bdcdb2b2b6894f31ff8455268bb9efb031cc7
Connor Wyatt <connorwyatt1@gmail.com>
2023-10-21 20:49:34 +0100

    Add rolling file logging

--- src/main.rs
+++ src/main.rs
@@ -20,12 +20,20 @@ use crate::todos::{
 async fn main() {
     dotenvy::dotenv().expect(".env file is missing");
 
+    let file_appender = tracing_appender::rolling::hourly("logs", "logs");
+    let (non_blocking_file_appender, _guard) = tracing_appender::non_blocking(file_appender);
+
     tracing_subscriber::registry()
         .with(
             tracing_subscriber::EnvFilter::try_from_default_env()
-                .unwrap_or_else(|_| "todos=debug".into()),
+                .unwrap_or_else(|_| "debug,hyper=warn".into()),
         )
-        .with(tracing_subscriber::fmt::layer())
+        .with(
+            tracing_subscriber::fmt::layer()
+                .json()
+                .with_writer(non_blocking_file_appender),
+        )
+        .with(tracing_subscriber::fmt::layer().with_writer(std::io::stdout))
         .init();
 
     let use_in_memory_repositories =
